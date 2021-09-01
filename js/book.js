e59dc2c88f422fd6045f65065c83f8a91b1ccb6c
Md-jaber-hossain <62210928+Md-jaber-hossain@users.noreply.github.com>
2021-09-02 05:36:08 +0600

    new add

--- js/book.js
+++ js/book.js
@@ -1,26 +1,26 @@
 const searchBook = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
+    // getting input value
+    const inputField = document.getElementById('input-field');
+    const inputValue = inputField.value;
+    // clear input value
+    inputField.value = '';
+    // clear search result and books count
     document.getElementById('search-result').textContent ='';
-    document.getElementById('book-numbers').textContent = '';
+    document.getElementById('book-count').textContent = '';
 
-    // clear data
-    searchField.value = '';
+    // chech the input field enpty or not
+    if (inputValue === '') {
 
-    if (searchText === '') {
-
-        displayError();
+        displayErrorEmpty();
     }
     else {
-        // Display Spinner
-        document.getElementById('spinner').style.display = 'block';
-        // Hide error
-        document.getElementById('error-message').style.display = 'none';
-        // Clear Search Result
+        // display the spinner
         document.getElementById('search-result').textContent ='';
-        // load data
-        const url = `https://openlibrary.org/search.json?q==${searchText}`;
-        console.log(url);
+        document.getElementById('spinner').style.display = 'block';
+        document.getElementById('error-message-empty').style.display = 'none';
+        document.getElementById('error-message-invalid').style.display = 'none';
+        // fetch the data and call the function
+        const url = `https://openlibrary.org/search.json?q==${inputValue}`;
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data.docs));
@@ -28,30 +28,49 @@ const searchBook = () => {
     }
 }
 
-const displayError = () => {
-    document.getElementById('error-message').style.display = 'block';
+// error message function for empty input value
+const displayErrorEmpty = () => {
+    document.getElementById('error-message-empty').style.display = 'block';
+    document.getElementById('error-message-invalid').style.display = 'none';
     document.getElementById('spinner').style.display = 'none';
-    document.getElementById('book-numbers').textContent = '';
+    document.getElementById('book-count').textContent = '';
 
 }
-// Display Search Result
+// error message function for invalid input value
+const displayErrorInvalid = () => {
+    document.getElementById('error-message-invalid').style.display = 'block';
+    document.getElementById('error-message-empty').style.display = 'none';
+    document.getElementById('spinner').style.display = 'none';
+    document.getElementById('book-count').textContent = '';
+
+}
+
+// getting the array books from search
 const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
 
-    console.log(books);
+    // chech the array book length
     if (books.length === 0) {
-        displayError();
+        displayErrorInvalid();
     }
     else {
-        document.getElementById('error-message').style.display = 'none';
+        // hide the error message and spinner
+        document.getElementById('error-message-empty').style.display = 'none';
+        document.getElementById('error-message-invalid').style.display = 'none';
         document.getElementById('spinner').style.display = 'none';
-        document.getElementById('book-numbers').innerText = `Books Found ${books.length}`;
-        // Retrieve each book and display in a card
+        // books count show
+        document.getElementById('book-count').innerText = `Books Found ${books.length}`;
+
+        // loop from array books
         books.forEach(book => {
+
+            // dynamically set the images
             const bookImg = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+            // create the div
             const div = document.createElement('div');
             div.classList.add('col');
+            // inside the div, set all the images and book details 
             div.innerHTML = `
             <div  class="card h-100 text-center">
                 <img src="${bookImg? bookImg: " "}" class="w-100 h-75 mx-auto" alt="...">
